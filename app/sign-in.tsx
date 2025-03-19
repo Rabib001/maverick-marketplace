e2ff320eea15ed68e5747bd02cb9288fc14017ff
Rabib001 <rxh3770@mavs.uta.edu>
2025-03-19 16:31:14 -0500

    Added demo assets folder and demo data in constants folder

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,11 @@
 import React from "react"
-import { View, Text } from "react-native"
+import { View, Text, SafeAreaView } from "react-native"
 
 const SignIn = () => {
     return (
-        <View>
+        <SafeAreaView>
             <Text>SignIn</Text>
-        </View>
+        </SafeAreaView>
     )
 }
 
